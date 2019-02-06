09449e40ba8db1b0ddb1f44fdbf37a5bfb7c4830
Cesare Camurani <cesarecamurani@Cesares-MacBook-Pro.local>
2019-02-06 14:00:43 +0000

    test for debit storing the transaction passing

--- test_transaction.py
+++ test_transaction.py
@@ -13,14 +13,24 @@ class TestTransaction(unittest.TestCase):
     def test_transactions_is_empty_at_init(self):
         assert self.transaction.transactions == []
 
-    def test_credit_stores_the_transaction(self):
+    def test_credit_stores_the_transaction_for_deposit(self):
         freezer = freeze_time("2012-01-14 12:00:01")
         freezer.start()
-        self.transaction.credit(1000, None, 2000)
+        self.transaction.credit(1000, 2000)
         self.assertEqual(self.transaction.transactions, [('date', datetime.datetime.now(),
                                                           'credit', 1000,
                                                           'debit', None,
                                                           'balance', 2000)])
+        freezer.stop()
+
+    def test_debit_stores_the_transaction_for_withdraw(self):
+        freezer = freeze_time("2012-01-14 12:00:01")
+        freezer.start()
+        self.transaction.debit(1000, 1000)
+        self.assertEqual(self.transaction.transactions, [('date', datetime.datetime.now(),
+                                                          'credit', None,
+                                                          'debit', 1000,
+                                                          'balance', 1000)])
         freezer.stop()
 
 if __name__ == '__main__':
--- transaction.py
+++ transaction.py
@@ -6,8 +6,14 @@ class Transaction:
     def __init__(self):
         self.transactions = []
 
-    def credit(self, credit, debit, balance):
+    def credit(self, credit, balance):
         self.transactions.append(('date', datetime.datetime.now(),
                                   'credit', credit,
                                   'debit', None,
+                                  'balance', balance))
+
+    def debit(self, debit, balance):
+        self.transactions.append(('date', datetime.datetime.now(),
+                                  'credit', None,
+                                  'debit', debit,
                                   'balance', balance))
