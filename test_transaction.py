d1748668769a90174988fe2d75850cae47468c9f
Cesare Camurani <cesarecamurani@Cesares-MacBook-Pro.local>
2019-02-06 13:55:51 +0000

    test for credit storing the transaction passing

--- test_transaction.py
+++ test_transaction.py
@@ -1,14 +1,10 @@
 import unittest
+from transaction import Transaction
 import datetime
 from mock import Mock
 from freezegun import freeze_time
-from transaction import Transaction
 
 class TestTransaction(unittest.TestCase):
-
-    @freeze_time("2018-06-06")
-    def test(self):
-        assert datetime.datetime.now() == datetime.datetime(2018, 6, 6)
 
     def setUp(self):
         self.bank_account = Mock()
@@ -17,8 +13,15 @@ class TestTransaction(unittest.TestCase):
     def test_transactions_is_empty_at_init(self):
         assert self.transaction.transactions == []
 
-    # def test_credit_stores_the_transaction(self):
-
+    def test_credit_stores_the_transaction(self):
+        freezer = freeze_time("2012-01-14 12:00:01")
+        freezer.start()
+        self.transaction.credit(1000, None, 2000)
+        self.assertEqual(self.transaction.transactions, [('date', datetime.datetime.now(),
+                                                          'credit', 1000,
+                                                          'debit', None,
+                                                          'balance', 2000)])
+        freezer.stop()
 
 if __name__ == '__main__':
     unittest.main()
--- transaction.py
+++ transaction.py
@@ -1,6 +1,13 @@
 from statement import Statement
+import datetime
 
 class Transaction:
 
     def __init__(self):
         self.transactions = []
+
+    def credit(self, credit, debit, balance):
+        self.transactions.append(('date', datetime.datetime.now(),
+                                  'credit', credit,
+                                  'debit', None,
+                                  'balance', balance))
