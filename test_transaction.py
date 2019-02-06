7594b96d03f96f101951939d1de5ea1554a72afa
Cesare Camurani <cesarecamurani@Cesares-MacBook-Pro.local>
2019-02-06 12:06:21 +0000

    test for mocking date passing

--- test_transaction.py
+++ test_transaction.py
@@ -1,8 +1,14 @@
 import unittest
+import datetime
 from mock import Mock
+from freezegun import freeze_time
 from transaction import Transaction
 
 class TestTransaction(unittest.TestCase):
+
+    @freeze_time("2018-06-06")
+    def test(self):
+        assert datetime.datetime.now() == datetime.datetime(2018, 6, 6)
 
     def setUp(self):
         self.bank_account = Mock()
@@ -10,6 +16,9 @@ class TestTransaction(unittest.TestCase):
 
     def test_transactions_is_empty_at_init(self):
         assert self.transaction.transactions == []
+
+    # def test_credit_stores_the_transaction(self):
+
 
 if __name__ == '__main__':
     unittest.main()
