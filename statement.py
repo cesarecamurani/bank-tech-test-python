b6d51b67d90ad6c9049fb6e26b6e2aac60184a70
Cesare Camurani <cesarecamurani@Cesares-MacBook-Pro.local>
2019-02-06 11:22:18 +0000

    test for header initialized in the right format passing

---
+++ statement.py
@@ -0,0 +1,4 @@
+class Statement:
+
+    def __init__(self):
+        self.header = " date || credit || debit || balance "
--- test_statement.py
+++ test_statement.py
@@ -1,5 +1,13 @@
 import unittest
+from statement import Statement
 
+class TestStatement(unittest.TestCase):
+
+    def setUp(self):
+        self.statement = Statement()
+
+    def test_header_format(self):
+        assert self.statement.header == " date || credit || debit || balance "
 
 if __name__ == '__main__':
     unittest.main()
